95d96d6fe0a79d0e962b70eb249f0a007d94f083
vidvaan <balajigvidvaan@gmail.com>
2024-04-15 09:48:31 +0530

    Add files via upload

---
+++ 013Spring-CiqTemplateDemo/src/main/java/com/ciq/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.ciq.controller;
+
+import java.util.List;
+
+import com.ciq.dto.Employee;
+import com.ciq.service.EmployeeService;
+
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public void setEmployeeService(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	public void save(Employee employee) {
+		System.out.println("Employee Controller Save Method");
+		employeeService.save(employee);
+	}
+
+	public void update(Employee employee) {
+		employeeService.update(employee);
+	}
+
+	public void deleteById(Integer eid) {
+		employeeService.deleteById(eid);
+	}
+
+}
